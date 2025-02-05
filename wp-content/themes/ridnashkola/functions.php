47ca263dc6e7ae35f9cb1e3cf44cf88fe0b5b21a
“Stanislav <w0859305@myscc.ca>
2025-02-05 10:38:59 -0500

    Initial Styles

--- wp-content/themes/ridnashkola/functions.php
+++ wp-content/themes/ridnashkola/functions.php
@@ -4,4 +4,65 @@ function educenter_child_enqueue_styles() {
     wp_enqueue_style('educenter-child-style', get_stylesheet_directory_uri() . '/style.css', array('educenter-parent-style'), wp_get_theme()->get('Version'));
 }
 add_action('wp_enqueue_scripts', 'educenter_child_enqueue_styles');
+
+/**
+ * Main header area
+ */
+if ( ! function_exists( 'educenter_main_header' ) ) {
+
+    function educenter_main_header() { ?>
+
+        <div class="bottom-header clearfix">
+            <div class="container">
+                <div class="header-middle-inner">
+                    <div class="site-branding logo">
+
+<!--                        --><?php //the_custom_logo(); ?>
+
+                        <div class="brandinglogo-wrap">
+                            <h1 class="site-title">
+                                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                                    <?php bloginfo( 'name' ); ?>
+                                </a>
+                            </h1>
+                            <?php
+                            $description = get_bloginfo( 'description', 'display' );
+                            if ( $description || is_customize_preview() ) : ?>
+                                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+                            <?php endif;  ?>
+                        </div>
+
+                        <button class="header-nav-toggle" data-toggle-target=".header-mobile-menu"  data-toggle-body-class="showing-menu-modal" aria-expanded="false" data-set-focus=".close-nav-toggle">
+                            <div class="one"></div>
+                            <div class="two"></div>
+                            <div class="three"></div>
+                        </button><!-- Mobile navbar toggler -->
+
+                    </div><!-- .site-branding -->
+
+                    <div class="box-header-nav main-menu-wapper">
+                        <?php
+                        wp_nav_menu( array(
+                                'theme_location'  => 'menu-1',
+                                'menu'            => 'primary-menu',
+                                'container'       => '',
+                                'container_class' => '',
+                                'container_id'    => '',
+                                'menu_class'      => 'main-menu',
+                            )
+                        );
+                        ?>
+                    </div>
+
+                    <?php do_action('educenter_nav_buttons'); ?>
+                </div>
+            </div>
+        </div>
+
+        <?php
+    }
+}
+add_action( 'educenter_header', 'educenter_main_header', 20 );
+
 ?>
+
